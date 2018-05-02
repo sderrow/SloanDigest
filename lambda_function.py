6ce0a414b8440c5afa2c4bd0c0d648faaa63f440
Sean Derrow <sean.derrow@gmail.com>
2018-05-02 16:38:58 -0400

    Commented code

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,9 @@
+# SloanDigest publishing script
+# MIT Sloan Product Innovation Hackathon
+# May 2018
+# Team 6: Sean Derrow, Raphael Korang, Matt Linderman,  Melinda Salaman
+# Code by Sean Derrow
+
 import boto3
 import json
 import uuid
@@ -7,24 +13,25 @@ from httplib2 import Http
 from oauth2client import file
 import pytz
 
+# Initialize the S3 bucket
 s3 = boto3.resource("s3")
 bucket = s3.Bucket("sloandigest")
 
 
-def load_sloangroups():
+def load_sloangroups():  # Load the SloanGroups JSON file stored on S3
     sloangroups_file = bucket.Object("sloangroups.json")
     txt = sloangroups_file.get()['Body'].read().decode('utf-8')
     txt_j = json.loads(txt)["value"]
     return txt_j
 
 
-def date_has_passed(d):
+def date_has_passed(d):  # Check to see if given date is in the past
     t = datetime.now(pytz.timezone('US/Eastern')).date()
     diff = d - t
     return diff.total_seconds() < 0
 
 
-def dt_formatter(dt):
+def dt_formatter(dt):  # Format datetime like 5/1/2018 = "Tuesday, May 1st"
     wkdy = dt.strftime("%A")
     mth = dt.strftime("%B")
     nub = int(dt.strftime("%d"))
@@ -39,20 +46,21 @@ def dt_formatter(dt):
     return "{}, {} {:d}{}".format(wkdy, mth, nub, ext)
 
 
-def parse_date(event_date):
+def parse_date(event_date):  # Put eventDate from SloanGroups JSON into desired format
     dt = datetime.strptime(event_date, "%Y-%m-%dT%H:%M:%S")
     return dt_formatter(dt)
 
 
-def parse_group(event_group):
+def parse_group(event_group): # Format the SloanGroups event "group" field to include a preceding "the" or not
     if event_group.lower()[-4:] == 'club':
         return "the {}".format(event_group)
     return event_group
 
 
-def craft_sloangroups():
+def craft_sloangroups():  # Build out the SloanGroups events readout of the briefing
     js = load_sloangroups()
 
+    # Check for stale events and start at the first fresh event (i.e., hasn't happened yet)
     i = 0
     for k, j in enumerate(js):
         i = k
@@ -105,13 +113,13 @@ def craft_sloangroups():
     return txt
 
 
-def setup_google_sheets():
+def setup_google_sheets():  # Set up the Google Sheets API service
     store = file.Storage('credentials.json')
     creds = store.get()
     return build('sheets', 'v4', http=creds.authorize(Http()))
 
 
-def scrape_key_academics():
+def scrape_key_academics():  # Scrape MIT key academic dates from corresponding Google Sheet
     global service
     service = setup_google_sheets()
     SPREADSHEET_ID = '1z1A4DQRTGwE4rzh5bpNXC85J8XPs3ZsWHVOgu_C-Ioo'
@@ -122,19 +130,19 @@ def scrape_key_academics():
     if not values:
         print('No data found.')
     else:
-        for row in values:
+        for row in values:  # Loop through events and choose the first one that has not yet passed
             d = datetime.strptime(row[0], "%m/%d/%Y").date()
             if not date_has_passed(d):
                 return d, row[1]
 
 
-def craft_key_academics_text():
+def craft_key_academics_text():  # Build out the MIT key academic dates part of the briefing
     nxt_date, event = scrape_key_academics()
     txt = "In key academic dates, the next big item is {} on {}. <break time=\"2s\"/> ".format(event, dt_formatter(nxt_date))
     return txt
 
 
-def load_meet_sloanie():
+def load_meet_sloanie():  # Scrape data from a single response to the "Meet a Sloanie" Google Sheet
     global service
     SPREADSHEET_ID = '1G6oJTyR7NVjN79JgW2Qf7cs2U7-EGkBL-e3LNW--hZE'
     RANGE_NAME = 'Form Responses 1!B2:N100'
@@ -144,12 +152,14 @@ def load_meet_sloanie():
     if not values:
         print('No data found.')
     else:
-        for row in values:
-            if row[-1] == "No" and row[-2] == "Yes":
-                return row[:-2]
+        for person in values:  # Loop through responses and choose the first one that is approved and has yet to be featured
+            featured_already = person[-1]
+            approved = person[-2]
+            if featured_already == "No" and approved == "Yes":
+                return person[:-2]
 
 
-def craft_meet_sloanie():
+def craft_meet_sloanie():  # Build out the "Meet a Sloanie" section of the briefing
     first, last, prog, yr, prev_job, prev_emp, town, fav1, fav2, pty1, pty2 = load_meet_sloanie()
     txt = "There are so many people to meet and learn from at Sloan! Today’s featured Sloanie is "
     txt += "{} {}! {} is a member of the {} class of {}. ".format(first, last, first, prog, yr)
@@ -159,16 +169,16 @@ def craft_meet_sloanie():
     return txt
 
 
-def create_main_text():
+def create_main_text():  # Combine all three sections to create the final text of the flash briefing
     msg = craft_sloangroups()
     msg += craft_key_academics_text()
     msg += craft_meet_sloanie()
     msg += "That's it for SloanDigest. Talk to you next time!"
-    msg = msg.replace("&", "and")
+    msg = msg.replace("&", "and")  # Alexa gets hung on reading the ampersand character, so replace it with "and"
     return msg
 
 
-def create_feed():
+def create_feed():  # Create the actual JSON that Alexa requires when reading out a feed
     uid = str(uuid.uuid4())
     current_datetime = datetime.utcnow()
     title = "SloanDigest First Cut"
@@ -184,8 +194,8 @@ def create_feed():
     return item
 
 
-def export_news_data(digest):
-    obj = bucket.Object("SloanDigest.JSON")
+def export_news_data(digest):  # Publish the JSON to the S3 bucket
+    obj = bucket.Object("SloanDigest.json")
     response = obj.put(
         ACL="public-read",
         Body=json.dumps(digest, ensure_ascii=False),
